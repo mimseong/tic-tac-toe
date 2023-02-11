4b89817c2c5562b7e1ddce1d7bc797707700dc95
mimseong <parkseongmin98@gmail.com>
2023-02-11 22:33:08 +0900

    Feat: prop 사용하는 방법
     - <Square value={i} />
     - this.props.value

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 class Square extends React.Component {
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return <button className="square">{this.props.value}</button>;
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
